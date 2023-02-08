a7c323b7a4584d736233baa630c4f7bd1a91b2fb
Matheus Balan <matheusbalanj5@gmail.com>
2023-02-08 15:57:07 -0300

    aula 3 até a 7

---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Ola Vue!!</h1>
+        <p>Meu nome é {{ nome }}, e trabalho como {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Primeiro Componente',
+        data(){
+            return{
+                nome: "Matheus",
+                profissao: "Programador"
+            }
+        }
+    }
+</script>
