08e4419a78edc836efd1bce5d80d0cc65567c047
Matheus Balan <matheusbalanj5@gmail.com>
2023-02-09 10:13:42 -0300

    Hierarquia de componentes

---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+    <div> 
+        <h2>Está é a descrição da pessoa: {{ nome }}</h2>
+        <p>Estou trabalhando no momento</p>
+        <p>Utilizo as seguintes tecnologias: </p>
+        <ul>
+            <li>C#</li>
+            <li>C++</li>
+            <li>JavaScript</li>
+            <li>SQLServer</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'Info'
+    }
+</script>
